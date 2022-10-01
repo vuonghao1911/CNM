b70e0e77b950a18586abed51be7526f1996b1555
Nguyen Vuong Hao <vuonghao14298@gmail.com>
2022-10-01 13:38:08 +0700

    validata form

--- route/route.js
+++ route/route.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const muller = require("multer");
 const convertFormToJson = muller();
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const { check, validationResult } = require("express-validator");
 
 function Router(app, doc) {
   //get home page
@@ -21,32 +24,60 @@ function Router(app, doc) {
   });
 
   //post data to server
-  router.post("/addPaper", convertFormToJson.fields([]), (req, res) => {
-    const { namePaper, author, year, pageNumber, isbn } = req.body;
-    console.log("data:%j ", req.body);
-    //return res.redirect("/");
-    const params = {
-      TableName: "Paper",
-
-      Item: {
-        id: new Date().getTime() + "dangbuoi",
-        tenBaiBao: namePaper,
-        tacGia: author,
-        isbn: isbn,
-        namXuatBan: year,
-        soTrang: pageNumber,
-      },
-    };
-
-    doc.put(params, (err, data) => {
-      if (err) {
-        return res.send("false");
-        console.log(err);
+  router.post(
+    "/addPaper",
+    convertFormToJson.fields([]),
+    urlencodedParser,
+    [
+      check("namePaper", "This Name must me 3+ characters long")
+        .exists()
+        .isLength({ min: 3 }),
+      check("author", "Author is not valid").exists().isLength({ min: 3 }),
+      check("isbn", "ISBN is XXX-XXX-XXX")
+        .exists()
+        .matches(/^[0-9]{3}-[0-9]{3}-[0-9]{3}$/),
+      check("pageNumber", "Pages is Number ")
+        .exists()
+        .isInt({ min: 1, max: 1000 }),
+      check("year", "Year is not valid")
+        .exists()
+        .isBefore(new Date().getFullYear().toString()),
+    ],
+    (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        const alert = errors.array();
+        return res.render("AddPaper", {
+          alert,
+        });
       } else {
-        return res.redirect("/");
+        const { namePaper, author, year, pageNumber, isbn } = req.body;
+        console.log("data:%j ", req.body);
+        //return res.redirect("/");
+        const params = {
+          TableName: "Paper",
+
+          Item: {
+            id: new Date().getTime() + "dangbuoi",
+            tenBaiBao: namePaper,
+            tacGia: author,
+            isbn: isbn,
+            namXuatBan: year,
+            soTrang: pageNumber,
+          },
+        };
+
+        doc.put(params, (err, data) => {
+          if (err) {
+            return res.send("false");
+            console.log(err);
+          } else {
+            return res.redirect("/");
+          }
+        });
       }
-    });
-  });
+    }
+  );
 
   router.post("/delete", convertFormToJson.fields([]), (req, res) => {
     const id = req.body.id;
